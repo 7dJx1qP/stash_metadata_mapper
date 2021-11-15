ca953ea9279afe42feed1409d74ba2cca5c673bd
7dJx1qP <38586902+7dJx1qP@users.noreply.github.com>
2021-11-14 20:21:42 -0500

    clear api_key

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJhZG1pbiIsImlhdCI6MTYzNjgwMzkxOCwic3ViIjoiQVBJS2V5In0.0V5-GS1tsIf34Ph-4EJ0kIkdcmKkTZ3xLHpjpUuCWbo"
+api_key = ""
 server_url = "http://localhost:9999/graphql"
